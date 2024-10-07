930a10b87dafb899cd0715b7dc155a2dcdf9b10f
ReWar1311 <prashantrewarofficial@gmail.com>
2024-10-08 05:28:06 +0530

    Adjusted latency and added starting Sound

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+var path ="./assets/blue.mp3";
+var audio= new Audio(path);
+var path1 ="./assets/1.mp3";
+var audio1= new Audio(path1);
+var path2 ="./assets/2.mp3";
+var audio2= new Audio(path2);
+var path3 ="./assets/3.mp3";
+var audio3= new Audio(path3);
+var path4 ="./assets/4.mp3";
+var audio4= new Audio(path4);
+var path5 ="./assets/5.mp3";
+var audio5= new Audio(path5);
+var path6 ="./assets/6.mp3";
+var audio6= new Audio(path6);
+var path7 ="./assets/7.mp3";
+var audio7= new Audio(path7);
+var path8 ="./assets/8.mp3";
+var audio8= new Audio(path8);
+var path9 ="./assets/9.mp3";
+var audio9= new Audio(path9);
 var array = [];
 gameOver();
 function gameOver(){
@@ -6,6 +26,7 @@ function gameOver(){
     document.addEventListener("click", restart);
 };
 function restart(){
+    audio.play();
     document.removeEventListener("keypress", restart);
     document.removeEventListener("click", restart);
     start(1);
@@ -14,7 +35,8 @@ function start(n) {
     var random = Math.floor(((Math.random()) * 9) + 1);
     array.push(random);
     setTimeout(function(){
-        clicked(array[n-1])     
+        var number= array[n-1]
+        clicked(number.toString())     
      },800)
     console.log(array, "hi");
     $("h1").text("LEVEL " + n);
@@ -84,9 +106,37 @@ function keyp(i, n) {
     }
 }
 function clicked(item){
-    var path ="./assets/"+item+".mp3";
-    var audio= new Audio(path);
-    audio.play();
+    switch(item) {
+        case "1":
+          audio1.play()
+          break;
+        case "2":
+          audio2.play()
+          break;
+        case "3":
+          audio3.play()
+          break;
+        case "4":
+          audio4.play()
+          break;
+        case "5":
+          audio5.play()
+          break;
+        case "6":
+          audio6.play()
+          break;
+        case "7":
+          audio7.play()
+          break;
+        case "8":
+          audio8.play()
+          break;
+        case "9":
+          audio9.play()
+          break;
+        default:
+          console.log(item)
+      }
     document.querySelector(".num"+item).classList.add("pressed");
     setTimeout(function(){
         document.querySelector(".num"+item).classList.remove("pressed");     
